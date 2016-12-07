7dab6a2e01ca8c4cff93c5116e1e7cf2aa904cce
Attila Gabor <gabor.attila87@gmail.com>
2016-12-07 14:39:15 +0100

    write license upon installing the package

--- install_VisId.m
+++ install_VisId.m
@@ -1,3 +1,4 @@
 % add the current working folder and subfolders to the MATLAB path
 addpath(genpath(cd))
-disp('VisId was added to the MATLAB path')
+disp('   VisId was added to the MATLAB path')
+fprintf('\n ***This program is distributed in the hope that it will be useful, but WITHOUT \n ANY WARRANTY; without even the implied warranty of MERCHANTABILITY\n or FITNESS FOR A PARTICULAR PURPOSE.\n See the GNU General Public License for more details.\n')
